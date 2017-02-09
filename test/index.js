5b60953d17a700c5c2a945b9a8f61386a187419d
Steven Soloff <steven.soloff@gmail.com>
2017-02-09 01:01:52 -0500

    Add relevant source lines to verify non-zero code coverage

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
+/*
+ * Copyright (C) 2017 Steven Soloff
+ *
+ * This software is licensed under the terms of the GNU Affero General Public
+ * License version 3 or later (https://www.gnu.org/licenses/).
+ */
+
 import assert from 'assert'
 import hazelslackServerCore from '../lib'
 
 describe('hazelslack-server-core', function () {
   it('should have unit test!', function () {
-    assert(hazelslackServerCore !== null, 'we expected this package author to add actual unit tests.')
+    assert(hazelslackServerCore.sum(1, 2) === 3, 'we expected this package author to add actual unit tests.')
   })
 })
